0c3e8b8848b32479b1c8729a4692b89a4fb11b0f
Brian Johnson Lin <brianjohnsonlin@gmail.com>
2018-08-17 18:59:12 -0700

    new character

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class PlayerAnimation : MonoBehaviour {
+	public bool loop;
+	public float frameSeconds = 1;
+	public Sprite[] sprites;
+
+	private SpriteRenderer spr;
+	private int frame = 0;
+	private float deltaTime = 0;
+     
+	// Use this for initialization
+	void Start () {
+		spr = GetComponent<SpriteRenderer> ();
+	}
+     
+    // Update is called once per frame
+	void Update () {
+		deltaTime += Global.deltaTime;
+		while (deltaTime >= frameSeconds) {
+			deltaTime -= frameSeconds;
+			frame++;
+			if (loop) {
+				frame %= sprites.Length;
+			} else if (frame >= sprites.Length) {
+				frame = sprites.Length - 1;
+			}
+		}
+		
+		spr.sprite = sprites[frame];
+    }
+}
