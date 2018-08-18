27418db5ed09319974d4131fb5c5d7cef3a3db93
Jonathan <jonjiang@ucdavis.edu>
2018-08-17 18:58:53 -0700

    goal can detect collision with player

---
+++ Assets/Scripts/GoalScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.name == "GoalTrigger") {
+			Debug.Log("You win");
+		}
+	}
+}
