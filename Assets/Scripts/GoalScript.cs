d0121716ee2288831f05469af7c773461c0c7955
Brian Johnson Lin <brianjohnsonlin@gmail.com>
2018-08-18 23:38:01 -0700

    fixed jons horrendous scene changing code

--- Assets/Scripts/GoalScript.cs
+++ Assets/Scripts/GoalScript.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GoalScript : MonoBehaviour {
+	private string[] LevelNames = {
+		"Tutorial1",
+		"Tutorial2",
+		"Tutorial3",
+		"Connor's Shitty 3rd level",
+		"Connor2",
+		"JonLevel1",
+		"Connor1",
+		"JonLevel2"
+	};
 
 	// Use this for initialization
 	void Start () {
@@ -18,30 +28,7 @@ public class GoalScript : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D col) {
 		if (col.name == "Player") {
 			string sceneName = SceneManager.GetActiveScene().name;
-			if (sceneName == "Tutorial1") {
-				SceneManager.LoadScene("Tutorial2");
-			}
-			if (sceneName == "Tutorial2") {
-				SceneManager.LoadScene("Tutorial3");
-			}
-			if (sceneName == "Tutorial3") {
-				SceneManager.LoadScene("Connor's Shitty 3rd level");
-			}
-			if (sceneName == "Connor's Shitty 3rd level") {
-				SceneManager.LoadScene("Connor2");
-			}
-			if (sceneName == "Connor2") {
-				SceneManager.LoadScene("JonLevel1");
-			}
-			if (sceneName == "JonLevel1") {
-				SceneManager.LoadScene("Connor1");
-			}
-			if (sceneName == "Connor1") {
-				SceneManager.LoadScene("JonLevel2");
-			}
-			if (sceneName == "JonLevel2") {
-				SceneManager.LoadScene("MainMenu");
-			}
+			SceneManager.LoadScene(System.Array.IndexOf(LevelNames, sceneName) + 1);
 		}
 	}
 }
